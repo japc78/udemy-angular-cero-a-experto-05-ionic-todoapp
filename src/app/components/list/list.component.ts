07f57d31067b50eabe451af39d515da7f86a6322
japc <japc.grafico@gmail.com>
2021-01-07 19:36:22 +0100

    Edit list title

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { AlertController, IonList } from '@ionic/angular';
 import { TodoService } from '../../services/todo.service';
 import { List } from '../../models/list.model';
 
@@ -12,9 +13,13 @@ export class ListComponent implements OnInit {
 
   @Input() isFinished = true;
 
+  // El componente hijo se captura los componentes Ionlist, como solo tiene uno sera el único, si hubiera mas seria un []
+  @ViewChild( IonList ) listTag: IonList;
+
   constructor(
     public todoService: TodoService,
-    private router: Router) {
+    private router: Router,
+    public alertController: AlertController) {
   }
 
   ngOnInit() {}
@@ -25,5 +30,45 @@ export class ListComponent implements OnInit {
 
   deleteList(list: List) {
     this.todoService.deleteList(list);
+  }
+
+  async editNameList(list: List) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Edit name',
+      inputs: [
+        {
+          name: 'title',
+          type: 'text',
+          placeholder: 'Name list',
+          value: list.title
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Test: Cancel action');
+            this.listTag.closeSlidingItems();
+          }
+        },
+        {
+          text: 'Update',
+          handler: (data) => {
+            console.log(data);
+            if (data.title.length === 0) {
+              return;
+            }
+
+            list.title = data.title;
+            this.todoService.saveStorage();
+            this.listTag.closeSlidingItems();
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 }
